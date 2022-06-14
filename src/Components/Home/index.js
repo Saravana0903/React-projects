755cf6c42230dd92fe0511f896e0dc3e0a8ba5b8
Saravana0903 <saravanaadiraju@gmail.com>
2022-06-14 11:28:30 +0000

    displaying nothing

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,1 +1,9 @@
-
+import "./index.css";
+const Home = () => {
+  return (
+    <div className="bg-home">
+      <h1 className="main-head"> Hi, Everyone</h1>
+    </div>
+  );
+};
+export default Home;
